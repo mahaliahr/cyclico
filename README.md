8b079e784a5e32d61fa8cffdb57f581f180a3dab
Glitch (cyclico) <none>
2019-12-08 12:15:55 +0000

    ⚽️🚞 Updated with Glitch

--- README.md
+++
@@ -1,4 +0,0 @@
-# dfpi-skills-finalproj
-final project for design skills module
-
-A 'Do-It-Yourself' audio responsive VJing tool made with P5js + ToneJS
--- sketch.js
+++ sketch.js
@@ -8,16 +8,19 @@ let playing = true;
 let dim;
 let circleSize;
 let thickness;
-
+let ready;
+let myFont;
 // Create a new canvas to the browser size
-async function setup() {
+function setup() {
+  ready = 0;
+
   if (mic) {
     mic.dispose();
     mic = null;
   } else {
     mic = new Tone.UserMedia();
 
-    await mic.open();
+    mic.open();
     console.log("opened microphone", mic.label);
 
     analyser = new AudioEnergy();
@@ -26,14 +29,17 @@ async function setup() {
     mic.connect(analyser);
     mic.connect(Tone.Master);
   }
- // Socket Connection Setup
-  socket = io.connect('http://localhost:42069');
-  // socket.on('mouse', newDrawing);
-  createCanvas(windowWidth, windowHeight);
 
-  colours = [color(255), color(0)];
-  bgColor = random(colours);
+  createCanvas(windowWidth, windowHeight);
+  console.log(createCanvas);
+  bgColor = color(255, 255, 255, 255);
   dim = min(width, height);
+
+  sliderOneChange(3);
+  sliderTwoChange(12);
+  sliderThreeChange(0.05);
+  // initialises alpha at 255
+  sliderFourChange(255);
 }
 
 // On window resize, update the canvas
@@ -42,109 +48,155 @@ function windowResized() {
 }
 
 function draw() {
-  // pattern parameters being controlled: thickness, size of circles,
- pattern(window.sliderOneValue * 10, window.sliderTwoValue / 2);
+  if (ready == 0) {
+    // console.log('fds');
+    showText();
+  } else if (ready == 1) {
+    //do nothing and wait
+  } else {
+    // pattern parameters being controlled: thickness, size of circles,
+    pattern(
+      window.sliderOneValue * 10,
+      window.sliderTwoValue * 10,
+      window.sliderThreeValue / 2,
+      window.sliderFourValue
+    );
+  }
 }
 
 // These control updating of default value of slider, based on user input
 function sliderOneChange(slider) {
   window.sliderOneValue = sliderOne.value;
-  console.log(window.sliderOneValue);
-  return window.sliderValue
 }
 
 function sliderTwoChange(slider) {
   window.sliderTwoValue = sliderTwo.value;
-  console.log(window.sliderTwoValue);
-  return window.sliderValue
 }
 
 function sliderThreeChange(slider) {
   window.sliderThreeValue = sliderThree.value;
-  console.log(window.sliderThreeValue);
-  return window.sliderValue
+  console.log(sliderThreeValue);
 }
 
-function buttonPress() {
-if (buttonPress) {
-  console.log('button pressed')
-  colours = [color(169,210,213),color(70,35,122),color(41,255,184), color(179,184,255), color(255,195,128), color(171,10,10), color(255,241,112)];
-  bgColor = random(colours);
+function sliderFourChange(slider) {
+  window.sliderFourValue = sliderFour.value;
+}
 
+function buttonOnePress() {
+  if (buttonOnePress) {
+    console.log("button pressed");
+    colours = [
+      color(169, 210, 213),
+      color(70, 35, 122),
+      color(41, 255, 184),
+      color(179, 184, 255),
+      color(255, 195, 128),
+      color(171, 10, 10),
+      color(255, 241, 112)
+    ];
+    bgColor = random(colours);
+  }
 }
+
+function buttonTwoPress() {
+  if (buttonTwoPress) {
+    console.log("button pressed");
+    colours = [color(255), color(0)];
+    bgColor = random(colours);
+  }
 }
-// function mouseMoved() {
-//   var data = {
-//     x: mouseX,
-//     y: mouseY
-//   }
-//   return false
-// }
+
+function loadFont() {
+  myFont = WebFont.load({
+    google: {
+      families: ["Overpass"]
+    }
+  });
+}
+
+function showText() {
+  push();
+  blendMode(BLEND);
+  fill("#d3d3d3");
+  textSize(40);
+  textFont("Overpass");
+  textAlign(CENTER);
+  text("input audio & click to begin", width / 2, height / 2);
+  pop();
+
+  ready = 1;
+}
 
 // This function creates the overall audio generated patterns
-function pattern(thickness, diameter = 0.25) {
- if (!mic || !analyser) return;
- analyser.update();
+function pattern(thickness, numberOfColumns = 100, diameter = 0.25, alpha) {
+  if (!mic || !analyser) {
+    return;
+  }
+  analyser.update();
 
-//controls the transparency of the colours
- bgColor.setAlpha(255 * (window.sliderThreeValue));
- //window.sliderThreeValue
- //mouseX/width
+  //controls the transparency of the colours
+  bgColor.setAlpha(alpha);
 
- if (playing) {
-   //using ToneJS to analyse different levels of the audio to use as parameters of control in the drawing
-   const bass = analyser.getEnergy(20, 150);
-   const basslevel = map(bass, -100, -30, 0, 100, true);
+  if (playing) {
+    //using ToneJS to analyse different levels of the audio to use as parameters of control in the drawing
+    const bass = analyser.getEnergy(20, 150);
+    const basslevel = map(bass, -100, -30, 0, 100, true);
 
-   const highmid = analyser.getEnergy(2600, 5200);
-   const highmidlevel = map(highmid, -100, -30, 0, 100, true);
+    const highmid = analyser.getEnergy(2600, 5200);
+    const highmidlevel = map(highmid, -100, -30, 0, 100, true);
 
-   const mid = analyser.getEnergy(400, 2600);
-   const midlevel = map(mid, -100, -30, 0, 100, true);
+    const mid = analyser.getEnergy(400, 2600);
+    const midlevel = map(mid, -100, -30, 0, 100, true);
 
-   let dim = min(width, height);
-   const time = millis() / 1000;
-   blendMode(BLEND);
+    let dim = min(width, height);
+    const time = millis() / 1000;
+    blendMode(BLEND);
 
-   xoff = xoff + 0.01;
-   let n = noise(xoff) * width;
-   const frequency = 0.1 * n * 0.001;
-   const v = sin(time * frequency);
-   const anim = v * 1 + 1;
+    xoff = xoff + 0.01;
+    let n = noise(xoff) * width;
+    const frequency = 0.1 * n * 0.001;
+    const v = sin(time * frequency);
+    const anim = v * 1 + 1;
 
-   background(bgColor);
-   blendMode(DIFFERENCE);
+    background(bgColor);
+    blendMode(DIFFERENCE);
 
-   drawEachTile(
-     width / 2,
-     height / 2,
-     dim * 0.5 * anim * (basslevel / 100),
-     220,
-     time * 0.5,
-     diameter
-   );
+    drawEachTile(
+      width / 2,
+      height / 2,
+      dim * 0.5 * anim * (basslevel / 100),
+      220,
+      time * 0.5,
+      diameter
+    );
 
-   drawEachTile(
-     width / 2,
-     height / 2,
-     dim * 0.9 * anim * (midlevel / 100),
-     180,
-     time * 0.5,
-     diameter
-   );
+    drawEachTile(
+      width / 2,
+      height / 2,
+      dim * 0.9 * anim * (midlevel / 100),
+      180,
+      time * 0.5,
+      diameter
+    );
 
-   drawEachTile(
-     width / 2,
-     height / 2,
-     dim * 0.2 * anim * (highmidlevel / 60),
-     80,
-     time * 0.5,
-     diameter
-   );
+    drawEachTile(
+      width / 2,
+      height / 2,
+      dim * 0.2 * anim * (highmidlevel / 60),
+      80,
+      time * 0.5,
+      diameter
+    );
 
-   drawColumns(width / 2, height / 2, basslevel * 2, 100, time * 0.2, thickness);
- }
-
+    drawColumns(
+      width / 2,
+      height / 2,
+      basslevel * 2,
+      numberOfColumns,
+      time * 0.2,
+      thickness
+    );
+  }
 }
 
 // Creates circle of circles
@@ -158,7 +210,6 @@ function drawEachTile(x, y, radius, sides = 3, angle = 0, diameter) {
     }
   }
 }
-
 // Creates circle of columns
 function drawColumns(x, y, radius, sides = 3, angle = 0, thickness) {
   for (let i = 0; i < sides; i++) {
@@ -178,7 +229,7 @@ function drawTile(sx, sy, diameter) {
   let n = noise(xoff) * width;
   const frequency = 0.1 * n * 0.000001;
   noStroke();
-  fill("fff");
+  fill(color(255, 255, 255));
   circleSize = dim * diameter;
   circle(sx + frequency, sy + frequency, circleSize, circleSize);
 }
@@ -188,12 +239,13 @@ function drawColumn(sx, sy, ex, ey, thickness) {
   const dim = min(width, height);
   noFill();
   strokeCap(SQUARE);
-  strokeWeight(thickness / dim * 100);
-  stroke("#fff");
+  strokeWeight((thickness / dim) * 100);
+  stroke(color(255, 255, 255));
   line(sx, sy, ex, ey);
 }
 
 function mousePressed() {
-
   Tone.start();
+
+  ready = 2;
 }
